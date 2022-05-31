5cb65f55b6d6dedb750b51ffae16c92a2fef845b
mfathony115 <mfathony115@gmail.com>
2022-05-31 11:33:13 +0700

    first

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <title>It's me - Muhammad Fathony</title>
+  <meta content="Description your self" name="description">
+  <meta content="Its me" name="keywords">
+
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i,900,900i" rel="stylesheet">
+
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <link href="assets/css/style.css" rel="stylesheet">
+</head>
+<body>
+
+  <header id="header" class="fixed-top ">
+    <div class="container d-flex align-items-center justify-content-between">
+      <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">It's me</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+    </div>
+  </header>
+
+  <div id="hero" class="home">
+    <div class="container">
+      <div class="hero-content">
+        <h1>I'm <span class="typed" data-typed-items="Muhammad Fathony, Fullstack Developer, Backend Developer, API developer"></span></h1>
+        <p>Fullstack, Backend, API</p>
+        <ul class="list-unstyled list-social">
+          <li><a href="https://www.facebook.com/tony.outsiders.7/"><i class="bi bi-facebook"></i></a></li>
+          <li><a href="https://twitter.com/jadiabayangbaik"><i class="bi bi-twitter"></i></a></li>
+          <li><a href="https://www.instagram.com/muhammad_fathony24/?hl=id"><i class="bi bi-instagram"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/muhammad-fathony-24a161216/"><i class="bi bi-linkedin"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <main id="main">
+    <div id="about" class="paddsection">
+      <div class="container">
+        <div class="row justify-content-between">
+          <div class="col-lg-4 ">
+            <div class="div-img-bg">
+              <div class="about-img">
+                <img src="assets/img/itsme.jpg" class="img-responsive" alt="me">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-7">
+            <div class="about-descr">
+              <p class="p-heading">Make technology a driver of better attitudes </p>
+              <p class="separator">love reading, studying, and explore of Indonesia nature. I'm a good leader in my family and a good father to my son. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div id="services">
+      <div class="container">
+        <div class="services-slider swiper" data-aos="fade-up" data-aos-delay="100">
+          <div class="swiper-wrapper">
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-briefcase"></i>
+                <span>Fullstack Developer</span>
+                <p class="separator">create enterprise resource system to tools store. Codeigniter as backend, Bootstrap, and javascript for frontend website.</p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-card-checklist"></i>
+                <span>Backend Developer</span>
+                <p class="separator">The recording cancer system in the hospital, with laravel technology as backend.</p>
+              </div>
+            </div>
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-bar-chart"></i>
+                <span>API Developer</span>
+                <p class="separator">API developer for absence system hospital, now more than 500 users used the applications. </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <div id="footer" class="text-center">
+    <div class="container">
+      <div class="socials-media text-center">
+
+        <ul class="list-unstyled">
+          <li><a href="https://www.facebook.com/tony.outsiders.7/"><i class="bi bi-facebook"></i></a></li>
+          <li><a href="https://twitter.com/jadiabayangbaik"><i class="bi bi-twitter"></i></a></li>
+          <li><a href="https://www.instagram.com/muhammad_fathony24/?hl=id"><i class="bi bi-instagram"></i></a></li>
+          <li><a href="https://www.linkedin.com/in/muhammad-fathony-24a161216/"><i class="bi bi-linkedin"></i></a></li>
+        </ul>
+
+      </div>
+
+      <p>&copy; Copyrights Folio. All rights reserved.</p>
+      <div class="credits">
+        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div>
+
+    </div>
+  </div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/js/main.js"></script>
+</body>
+</html>
